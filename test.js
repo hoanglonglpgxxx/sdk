47eea4353db46199b72a281ae7e3d6491cb0c0f0
Mits <hoanglonglpgxxx@gmail.com>
2024-05-31 17:30:18 +0700

    up

--- test.js
+++ test.js
@@ -9,7 +9,13 @@ class ChatWindow {
         this.file = config.file || null;
 
         this.loadCSS('/sample.css');
-
+        // this.getParams();
+        /* let allParams = this.getQueryParams();
+        allParams.forEach(param => {
+            console.log(this);
+            this[param.key] = param.value;
+        });
+ */
         /* if (VHV) {
             console.log(VHV.getTime());
         } */
@@ -25,10 +31,34 @@ class ChatWindow {
 
         let p = `<div id="chat-GPT" class="chatGPT-icon chatbot-icon"><a href="javascript:void(0);" class="chatGPT-icon-action"><img alt="ChatIcon" src="${this.chatGPTImg}" height="80" /></a><span class="x-botchat" id="x-botchat"><svg width="16" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.729 5.285 C 5.520 5.388,5.294 5.645,5.233 5.848 C 5.128 6.197,5.025 6.078,8.113 9.170 L 10.939 12.000 8.113 14.830 C 5.009 17.938,5.128 17.801,5.237 18.165 C 5.304 18.388,5.618 18.700,5.835 18.759 C 6.214 18.861,6.061 18.993,9.170 15.887 L 12.000 13.061 14.830 15.887 C 17.939 18.993,17.786 18.861,18.165 18.759 C 18.386 18.699,18.699 18.386,18.759 18.165 C 18.861 17.786,18.993 17.939,15.887 14.830 L 13.061 12.000 15.887 9.170 C 18.993 6.061,18.861 6.214,18.759 5.835 C 18.700 5.618,18.388 5.304,18.165 5.237 C 17.801 5.128,17.938 5.009,14.830 8.113 L 12.000 10.939 9.190 8.131 C 7.229 6.172,6.335 5.305,6.231 5.262 C 6.033 5.179,5.933 5.184,5.729 5.285 " stroke="none" fill-rule="evenodd" fill="black"></path></svg></span></div><div id="chat-window" class="chat-window"></div>`;
         document.body.innerHTML += p;
-
+        console.log(123123123);
         this.initEvents();
 
         // this.initUsingIframe(); //TEST CASE TẠO IFRAME
+    }
+
+    getQueryParams() {
+        let params = new URLSearchParams(window.location.search);
+        let result = [];
+
+        for (let param of params) {
+            result.push({
+                key: param[0],
+                value: param[1]
+            });
+        }
+
+        return result;
+    }
+
+    getParams() {
+        let scriptUrl = document.querySelector('script[src*="test.js"]').src;
+        let url = new URL(scriptUrl);
+
+        this.siteId = url.searchParams.get('siteId');
+        this.chatGPTImg = url.searchParams.get('chatGPTImg');
+        this.title = url.searchParams.get('title');
+        this.roomId = url.searchParams.get('roomId');
     }
 
     loadCSS(val) {
@@ -64,7 +94,8 @@ class ChatWindow {
         let that = this,
             chatGPTIcon = document.querySelector('.chatGPT-icon'),
             xBotChat = document.querySelector('.x-botchat'),
-            chatWindow = document.getElementById('chat-window');
+            chatWindow = document.getElementById('chat-window'),
+            iframe = document.createElement('iframe');
         chatGPTIcon.addEventListener('click', function (e) {
             e.stopPropagation();
             if (!document.querySelector('.window-chatbot')) {
@@ -443,83 +474,6 @@ class ChatWindow {
         var iframe = document.createElement('iframe');
         const chatActions = `<div class="list-action-chat full-width d-flex relative"><div id="input-message" contenteditable="true" class="message-content border-solid border-gray-lighten message-content" name="title"></div><div><button type="button" class="submit submit-button{mid}"> <svg width="16" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.249 2.286 C 1.671 2.389,1.266 2.892,1.274 3.494 C 1.279 3.866,1.255 3.789,2.750 8.245 L 4.020 12.030 3.914 12.345 C 3.856 12.518,3.245 14.334,2.556 16.380 C 1.370 19.904,1.303 20.118,1.288 20.444 C 1.274 20.730,1.285 20.819,1.355 20.977 C 1.490 21.283,1.661 21.465,1.941 21.603 C 2.156 21.709,2.241 21.729,2.469 21.727 C 2.791 21.723,3.060 21.635,3.880 21.269 C 16.421 15.657,21.698 13.292,21.877 13.201 C 22.394 12.938,22.597 12.708,22.708 12.257 C 22.766 12.022,22.766 11.978,22.708 11.743 C 22.597 11.292,22.394 11.061,21.880 10.801 C 21.462 10.589,3.379 2.508,3.080 2.399 C 2.737 2.275,2.494 2.242,2.249 2.286 M11.970 7.992 C 16.870 10.185,20.880 11.989,20.880 12.001 C 20.880 12.035,2.955 20.048,2.931 20.024 C 2.919 20.013,3.457 18.378,4.126 16.392 L 5.342 12.780 8.121 12.760 L 10.900 12.740 11.051 12.646 C 11.270 12.510,11.338 12.357,11.339 12.000 C 11.340 11.642,11.246 11.452,11.009 11.334 C 10.869 11.264,10.695 11.259,8.094 11.248 L 5.328 11.237 4.124 7.637 C 3.462 5.657,2.920 4.018,2.920 3.994 C 2.920 3.970,2.950 3.963,2.990 3.978 C 3.029 3.993,7.070 5.799,11.970 7.992 " stroke="none" fill-rule="evenodd" fill="black"></path></svg></button></div></div>`;
 
-        /* iframe.srcdoc = `
-        <div class="window-chatGPT">
-            <div class="header padding-h-lg padding-v-sm align-center background-black  ">
-                <div class="header-img">
-                <img
-                    src="${this.chatGPTImg ? this.chatGPTImg : '/Extra/ChatGPT/images/small-icon.png'}"
-                    onerror="this.src='/Extra/ChatGPT/images/small-icon.png';"
-                    alt=""
-                />
-                </div>
-                <div class="header-title text-bold padding-h-sm title-md">Chat GPT</div>
-                <div class="header-actions d-flex  ">
-                <a
-                    class="   action-button  "
-                    href="javascript:void(0)"
-                    data-x-service="fullWindow"
-                    >
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.840 2.238 C 14.833 2.259,14.736 2.262,14.604 2.340 C 14.419 2.449,14.318 2.596,14.277 2.815 C 14.208 3.182,14.331 3.499,14.604 3.660 C 14.735 3.737,14.823 3.740,16.957 3.752 L 19.175 3.764 16.231 6.712 C 12.996 9.951,13.139 9.785,13.241 10.165 C 13.301 10.386,13.614 10.699,13.835 10.759 C 14.215 10.861,14.049 11.004,17.288 7.769 L 20.236 4.825 20.248 7.043 C 20.260 9.177,20.263 9.265,20.340 9.396 C 20.449 9.581,20.596 9.682,20.815 9.723 C 21.182 9.792,21.499 9.669,21.660 9.396 C 21.738 9.264,21.741 9.169,21.760 6.100 C 21.771 4.362,21.766 2.884,21.750 2.815 C 21.677 2.518,21.386 2.264,21.080 2.231 C 21.003 2.223,19.545 2.226,17.840 2.238 M9.800 13.245 C 9.691 13.277,9.012 13.933,6.710 16.232 L 3.760 19.179 3.758 17.079 C 3.757 15.925,3.741 14.902,3.723 14.806 C 3.653 14.443,3.418 14.268,3.000 14.268 C 2.582 14.268,2.347 14.444,2.278 14.806 C 2.259 14.902,2.243 16.380,2.240 18.091 L 2.235 21.203 2.328 21.370 C 2.429 21.552,2.621 21.702,2.815 21.750 C 2.884 21.766,4.362 21.771,6.100 21.760 C 9.169 21.741,9.264 21.738,9.396 21.660 C 9.581 21.551,9.682 21.404,9.723 21.185 C 9.792 20.818,9.669 20.501,9.396 20.340 C 9.265 20.263,9.177 20.260,7.043 20.248 L 4.825 20.236 7.769 17.288 C 10.399 14.655,10.718 14.322,10.759 14.169 C 10.893 13.658,10.312 13.097,9.800 13.245 " stroke="none" fill-rule="evenodd" fill="black"></path></svg></a>
-                <a
-                    class="   action-button  "
-                    href="javascript:void(0)"
-                    data-x-service="closeWindow"
-                    data-target="#chat-GPT{quote(gridModuleParentId)}"
-                    >
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.729 3.285 C 3.520 3.388,3.294 3.645,3.233 3.848 C 3.127 4.202,2.932 3.985,7.113 8.170 L 10.939 12.000 7.113 15.830 C 2.914 20.033,3.126 19.796,3.237 20.165 C 3.304 20.388,3.618 20.700,3.835 20.759 C 4.219 20.862,3.962 21.092,8.170 16.887 L 12.000 13.061 15.830 16.887 C 20.038 21.092,19.781 20.862,20.165 20.759 C 20.386 20.699,20.699 20.386,20.759 20.165 C 20.862 19.781,21.092 20.038,16.887 15.830 L 13.061 12.000 16.887 8.170 C 21.092 3.962,20.862 4.219,20.759 3.835 C 20.700 3.618,20.388 3.304,20.165 3.237 C 19.796 3.126,20.033 2.914,15.830 7.113 L 12.000 10.939 8.190 7.131 C 5.506 4.449,4.336 3.305,4.231 3.262 C 4.033 3.179,3.933 3.184,3.729 3.285 " stroke="none" fill-rule="evenodd" fill="black"></path></svg></a>
-                </div>
-            </div>
-            <div class="body scroll-container">
-                <div class="content" id="loadModuleChatDetail" style="position: relative">
-                <div
-                    id="list-message-chat"
-                    class="message-scroller list-message-chat padding-lg"
-                    data-id="{quote(!empty(roomId) ? roomId : request('roomId'))}"
-                >
-                    <Extra.ChatGPT.itemChat></Extra.ChatGPT.itemChat>
-                </div>
-                </div>
-            </div>
-            <div class="footer border-top border-width-base border-gray-lighten">
-                <form
-                id="form{quote(mid)}"
-                class="form d-flex message-form flex-column"
-                style="flex-direction: column"
-                >
-                <input type="hidden" name="roomId" value="{!empty(roomId)?quote(roomId):''}" />
-                ${chatActions}
-                </form>
-            </div>
-        </div>`
-            ;
-    */
-
-        iframe.srcdoc = `<div id="chat-GPT" class="chatGPT-icon">
-            <a href="javascript:void(0);" class="chatGPT-icon-action" >
-                <img alt=""
-                     src="/big-icon.png"
-                     onerror="this.src='/big-icon.png';" />
-            </a>
-            <span class="x-botchat"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.729 5.285 C 5.520 5.388,5.294 5.645,5.233 5.848 C 5.128 6.197,5.025 6.078,8.113 9.170 L 10.939 12.000 8.113 14.830 C 5.009 17.938,5.128 17.801,5.237 18.165 C 5.304 18.388,5.618 18.700,5.835 18.759 C 6.214 18.861,6.061 18.993,9.170 15.887 L 12.000 13.061 14.830 15.887 C 17.939 18.993,17.786 18.861,18.165 18.759 C 18.386 18.699,18.699 18.386,18.759 18.165 C 18.861 17.786,18.993 17.939,15.887 14.830 L 13.061 12.000 15.887 9.170 C 18.993 6.061,18.861 6.214,18.759 5.835 C 18.700 5.618,18.388 5.304,18.165 5.237 C 17.801 5.128,17.938 5.009,14.830 8.113 L 12.000 10.939 9.190 8.131 C 7.229 6.172,6.335 5.305,6.231 5.262 C 6.033 5.179,5.933 5.184,5.729 5.285 " stroke="none" fill-rule="evenodd" fill="black"></path></svg></span>
-        </div>
-        <script>
-        var chatGPTIcons = document.querySelectorAll('.chatGPT-icon');
-            console.log(chatGPTIcons);
-            chatGPTIcons.forEach(function (icon) {
-                icon.addEventListener('click', function () {
-                    console.log('12312fasdasd3');
-                    if (!document.querySelector('.window-chatbot')) {
-                        document
-                            .getElementById('chat-window')
-                            .appendChild(this.createChatWindow());
-                    }
-                });
-            });
-        </script>
-        <div id="chat-window" class="chat-window"></div>
-        `;
-
         document.body.appendChild(iframe);
     }
 
@@ -540,4 +494,6 @@ class ChatWindow {
     }
 }
 
-
+window.onload = function () {
+    window.chatbotAsyncInit();
+};
